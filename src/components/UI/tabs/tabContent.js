03098d245f61bf062a6b683b4bbff2c133157ca4
Saiyam <saiyam@vinsol.com>
2021-03-01 13:06:01 +0530

    Added Proptypes and defaultProps

--- src/components/UI/tabs/tabContent.js
+++ src/components/UI/tabs/tabContent.js
@@ -1,3 +1,13 @@
+import PropTypes from 'prop-types';
+
 const tabContent = (props) => props.children;
+
+tabContent.defaultProps = {
+  children: ''
+};
+
+tabContent.propTypes = {
+  children: PropTypes.node
+};
 
 export default tabContent;
